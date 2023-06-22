1be7ac256b18da415b7a2d3205ad24aab253e8a9
dseydahmetova <dseydahmetova@gmail.com>
2023-06-22 18:50:30 -0400

    commit - m "code improvements"

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        //constants
         final byte monthsInYear = 12;
         final byte percent = 100;
 
