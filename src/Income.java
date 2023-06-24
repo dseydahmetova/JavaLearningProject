42d753daa5da56f255777254ee0eface5bed3452
dseydahmetova <dseydahmetova@gmail.com>
2023-06-24 10:36:13 -0400

    if condition

---
+++ src/Income.java
@@ -0,0 +1,11 @@
+public class Income {
+
+    public static void main(String[] args) {
+        int income = 120_000;
+        boolean hasHighIncome;
+        if (income > 100_000)
+            hasHighIncome = true;
+        else
+            hasHighIncome = false;
+    }
+}
