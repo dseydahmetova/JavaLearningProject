760dbd4ee6a252e299dc281484a9f8993a282329
dseydahmetova <dseydahmetova@gmail.com>
2023-06-24 10:41:12 -0400

    ternary operator

--- src/Income.java
+++ src/Income.java
@@ -2,6 +2,6 @@ public class Income {
 
     public static void main(String[] args) {
         int income = 120_000;
-        boolean hashHighIncome = (income > 100_000);
+        String className = income > 100_000 ? "First" : "Economy";
     }
 }
