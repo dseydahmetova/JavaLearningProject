8c016fce4e50297d4620a0c816c4704a28c322cc
dseydahmetova <dseydahmetova@gmail.com>
2023-06-24 10:37:33 -0400

    improvement of the code

--- src/Income.java
+++ src/Income.java
@@ -2,10 +2,6 @@ public class Income {
 
     public static void main(String[] args) {
         int income = 120_000;
-        boolean hasHighIncome;
-        if (income > 100_000)
-            hasHighIncome = true;
-        else
-            hasHighIncome = false;
+        boolean hashHighIncome = (income > 100_000);
     }
 }
