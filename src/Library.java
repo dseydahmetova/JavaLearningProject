7c1ebc6b2b40d0bcef5a7452d20bb67c746c5090
dseydahmetova <dseydahmetova@gmail.com>
2023-06-26 10:41:03 -0400

    created user and book classes

--- src/Library.java
+++ src/Library.java
@@ -6,8 +6,14 @@ public class Library {
         user.name = "Farhan junior";
         user.birthDay = LocalDate.parse("1995-01-31");
 
-        System.out.printf("%s was born in %s and he is %d years old.\n", user.name, user.birthDay, user.age());
+        Book book = new Book();
+        book.title = "Carmilla";
+        book.author = "Sheridan le Fanu";
 
+        user.borrow(book);
+
+        System.out.printf("%s was born in %s and he is %d years old.\n", user.name, user.birthDay, user.age());
+        System.out.printf("%s has borrowed these books: %s \n", user.name, user.books);
 
     }
 }
