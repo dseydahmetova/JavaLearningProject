98d7bdfb6e81e1b612743313d2ef58d28e78644a
dseydahmetova <dseydahmetova@gmail.com>
2023-07-02 22:22:26 -0400

    corrections to hashset

--- src/Hash.java
+++ src/Hash.java
@@ -1,16 +1,18 @@
 import java.util.*;
 public class Hash {
-
+    // The use of a HashSet allows for efficient lookup of elements, improving the overall time complexity to O(N) instead of O(N^2) in the previous implementation.
         public int solve(ArrayList<Integer> A, Integer B) {
             if (B == null) {
                 return 0;
             } else {
                 HashSet<Integer> set = new HashSet<>();
                 for (int num : A) {
+                    set.add(num); // Add elements of the array to the HashSet
+                }
+                for (int num : A) {
                     if (set.contains(num + B) || set.contains(num - B)) {
                         return 1; // Found a pair with the required difference
                     }
-                    set.add(num);
                 }
             }
             return 0;
