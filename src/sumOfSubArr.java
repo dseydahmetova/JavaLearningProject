64961eff46e7c13a4f9a76bf74bbfafb9c38bd7a
dseydahmetova <dseydahmetova@gmail.com>
2023-07-17 21:48:25 -0400

    sum defined

---
+++ src/sumOfSubArr.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+//
+//We define the following:
+//
+//        A subarray of an -element array is an array composed from a contiguous block of the original array's elements. For example, if , then the subarrays are , , , , , and . Something like  would not be a subarray as it's not a contiguous subsection of the original array.
+//        The sum of an array is the total sum of its elements.
+//        An array's sum is negative if the total sum of its elements is negative.
+//        An array's sum is positive if the total sum of its elements is positive.
+//        Given an array of  integers, find and print its number of negative subarrays on a new line.
+//
+//        Input Format
+//
+//        The first line contains a single integer, , denoting the length of array .
+//        The second line contains  space-separated integers describing each respective element, , in array .
+//
+//        Constraints
+//
+//        Output Format
+//
+//        Print the number of subarrays of  having negative sums.
+//
+//        Sample Input
+//
+//        5
+//        1 -2 4 -5 1
+//        Sample Output
+//
+//        9
+public class sumOfSubArr {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int tot = arr[i];
+            if (tot < 0) {
+                count += 1;
+            }
+            for (int j = i + 1; j < n; j++) {
+                tot += arr[j];
+                if (tot < 0) {
+                    count += 1;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
